f0b5c75390aa306582701f704b77fc29b4ec188f
João Pedro <jpgomes06@hotmail.com>
2023-04-17 21:21:05 -0300

    test: adding test to validate an error message when creating user

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,20 +8,24 @@ it('should create an user', async () => {
     
     const response = await axios.post("http://localhost:3000/users", input);
     
-    expect(response.data.name).toEqual("João pedro")
+    expect(response.data.name).toEqual("João Pedro")
 });
 
 it('should not create an user', async () => {
     const input = {
-        "name": "João Pedro"
+        "name": "João Pedro",
+        "email": ""  
     } 
     
-    const response = await axios.post("http://localhost:3000/users", input);
-    
-    expect(response.data.name).toEqual("João Pedro")
-    expect(response.data.email).toEqual("jpgomes06@hotmail.com")
+    const response = await axios.post('http://localhost:3000/users', input, {// url fazendo uma requisição usando os dados do input
+        validateStatus: function (status) {
+            return status;
+        },
+    });
 
+    expect(response.status).toEqual(400)
+    expect(response.data.message).toEqual("Name and e-mail are necessary")
+       
 });
-
 
 
