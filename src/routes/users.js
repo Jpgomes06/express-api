1faa853ee493e4458b0c38420948bad991f9c454
João Pedro <jpgomes06@hotmail.com>
2023-04-25 19:11:42 -0300

    feat: adding fake user into users array

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();// ferramenta do express que permite criar rotas em diversas rotas e usar no index.js
 const crypto = require('crypto');
 
-const users = [];
+const users = 
+[
+    {
+        "id": "ec493503-eada-410d-b8e0-1e6674edfe11", 
+        "name": "Matheus Martins",
+        "email": "matheus_martins@hotmail.com"
+    }
+];
 
 router.get("/", (req, res) => {
         return res.json(users);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => { 
     const { id } = req.params;  
     
     const userIndex = users.findIndex((user) => user.id == id);    
@@ -29,6 +36,7 @@ router.put("/:id", (req, res) => {
     const { name, email } = req.body;
 
     const newUSer = {
+        // id: "ec493503-eada-410d-b8e0-1e6674edfe11",
         id,
         name, 
         email
@@ -44,7 +52,7 @@ router.put("/:id", (req, res) => {
         })
     }
 
-    return res.json(newUSer);
+    return res.status(201).json(newUSer);
 });
 
 router.post("/", (req, res) => {
@@ -60,7 +68,7 @@ router.post("/", (req, res) => {
             return res.status(400).json({ message: "Name and e-mail are necessary" });
         }
     
-        users.push(user);
+        users.push(user); 
             
         return res.status(201).json(user);
 });
@@ -72,7 +80,7 @@ router.delete("/:id",(req, res) => {
     
     if( userIndex !== -1 ){
         users.splice(userIndex, 1);
-        return res.status(202).json({
+        return res.status(202).json({ 
             success: true
         })        
     }
